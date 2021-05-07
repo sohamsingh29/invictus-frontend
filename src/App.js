016d97c09901ef99b09ba755def7463e8e8317f7
sohamsingh29 <sohamsingh29@gmail.com>
2021-05-07 19:03:08 +0530

    some updations

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import React, { useState } from "react";
-import { Box, Button, Container, Heading, Input, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  Heading,
+  Input,
+  Text,
+} from "@chakra-ui/react";
 function App() {
   const [eInput, setEInput] = useState("");
+  const [before, setBefore] = useState("");
   const [dInput, setDInput] = useState("");
   const [result, setResult] = useState("");
   const handleEncode = (e) => {
     e.preventDefault();
+    setBefore(eInput);
     fetch(`https://invictus-backend.vercel.app/api/encode/${eInput}`)
       .then((res) => res.json())
       .then((data) => setResult(data.encoded), setEInput(""));
   };
   const handleDecode = (e) => {
     e.preventDefault();
+    setBefore(dInput);
+
     fetch("https://invictus-backend.vercel.app/api/decode/" + dInput)
       .then((res) => res.json())
       .then((data) => setResult(data.decoded), setDInput(""));
   };
+
+  const clearState = () => {
+    setBefore("");
+    setDInput("");
+    setResult("");
+    setEInput("");
+  };
   return (
     <Container centerContent>
       <Box marginY="10" width="xl" rounded="md" shadow="xl" p="10">
-        {result.length > 0 && <Text> Encode/decode your string</Text>}
+        {result.length > 0 && (
+          <Flex justify="space-between">
+            <Text>given string : </Text>
+            <Text decoration="underline">{before}</Text>
+            <Button onClick={clearState}>Clear</Button>
+          </Flex>
+        )}
+
         <Heading my="5">
           {result.length ? "Answer : " + result : "Encode / Decode your String"}
         </Heading>
